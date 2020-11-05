064ff04861de311411d9e987166702a1979e4283
ACatpinar <acatpinar@windowslive.com>
2020-11-04 19:58:41 -0500

    OOP -- Abstraction

---
+++ src/main/java/com/OOP/Abstraction/Implementation/UserServiceImplementation.java
@@ -0,0 +1,14 @@
+package com.OOP.Abstraction.Implementation;
+
+import com.OOP.Encapsulation.User;
+
+public interface UserServiceImplementation {
+
+
+    String userByRoleId(int id);
+    User userByFirstName(String firstName);
+
+
+
+
+}
---
+++ src/main/java/com/OOP/Abstraction/Service/UserService.java
@@ -0,0 +1,22 @@
+package com.OOP.Abstraction.Service;
+
+import com.OOP.Abstraction.Implementation.UserServiceImplementation;
+import com.OOP.Encapsulation.Role;
+import com.OOP.Encapsulation.User;
+
+public class UserService implements UserServiceImplementation {
+
+
+    @Override
+    public String userByRoleId(int id) {
+        User user = new User("Mike","Smith",new Role(1,"Admin"));
+        return user.getFirstName()+" "+user.getLastName();
+    }
+
+    @Override
+    public User userByFirstName(String firstName) {
+        User user = new User("Tom","Ally",new Role(15,"Employee"));
+        return user;
+
+    }
+}

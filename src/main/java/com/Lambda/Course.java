4d53ada1e4d1cb814e3fc1eec6931e97d72bf1d9
ACatpinar <acatpinar@windowslive.com>
2020-11-04 20:53:35 -0500

    Lambda

---
+++ src/main/java/com/Lambda/Course.java
@@ -0,0 +1,7 @@
+package com.Lambda;
+
+public interface Course {
+
+    void study();
+
+}
---
+++ src/main/java/com/Lambda/Selenium.java
@@ -0,0 +1,9 @@
+package com.Lambda;
+
+public class Selenium implements Course {
+
+    @Override
+    public void study() {
+        System.out.println("Studying Selenium");
+    }
+}

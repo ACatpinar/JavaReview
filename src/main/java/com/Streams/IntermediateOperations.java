92c68f55e5fec2754dbcdeb9e170faa5551b33db
ACatpinar <acatpinar@windowslive.com>
2020-11-14 14:37:01 -0500

    Stream Operations

--- src/main/java/com/Streams/IntermediateOperations.java
+++ src/main/java/com/Streams/IntermediateOperations.java
@@ -1,6 +1,7 @@
 package com.Streams;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -23,6 +24,30 @@ public class IntermediateOperations {
         //System.out.println(stringStream.filter(str -> str.startsWith("A")).count()); //Error
 
         Arrays.asList("Apple","Orange","Kiwi","Banana","Apple","Cherry","Mango").stream().filter(str-> str.equals("Apple")).forEach(str-> System.out.println(str));
+
+        System.out.println("*************MAP******************");
+
+        Arrays.asList("Apple","Orange","Kiwi","Banana","Apple","Cherry","Mango").stream().map(str-> str.toUpperCase()).forEach(str-> System.out.println(str));
+
+        System.out.println("*************Distinct******************");
+
+        Arrays.asList("Apple","Orange","Kiwi","Banana","Apple","Cherry","Mango").stream().distinct().forEach(str-> System.out.println(str));
+
+        System.out.println("*************Sorted Order******************");
+
+        Arrays.asList(1,3,2,4,5,6,7,8,9).stream().sorted().forEach(str-> System.out.println(str));
+
+        System.out.println("************* Reverse Sorted Order******************");
+
+        Arrays.asList(1,3,2,4,5,6,7,8,9).stream().sorted(Comparator.reverseOrder()).forEach(number-> System.out.println(number));
+
+
+
+
+
+
+
+
 
 
 
